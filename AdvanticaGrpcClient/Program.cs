f9a3bf2684d568c9d0c6c28603c7c89d42723869
Dmitry K <augustustenerus@gmail.com>
2022-04-02 12:24:41 +0400

    Adding empty console client project

---
+++ AdvanticaGrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvanticaGrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
