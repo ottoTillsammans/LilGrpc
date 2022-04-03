585f04d505aeb7f5bbb1f15295bb753c57352593
Dmitry K <augustustenerus@gmail.com>
2022-04-03 13:50:14 +0400

    Implementation of server and simple console client

--- AdvanticaGrpcClient/Program.cs
+++ AdvanticaGrpcClient/Program.cs
@@ -1,12 +1,160 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using AdvanticaGrpcServer;
+using System;
+using System.Threading.Tasks;
 
 namespace AdvanticaGrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string create = "create";
+            const string read = "read";
+            const string update = "update";
+            const string delete = "delete";
+            const string exit = "exit";
+
+            using var defaultChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(defaultChannel);
+            string type = string.Empty;
+
+            while (true)
+            {
+                Console.WriteLine("Input the command type: \"create\", \"read\", \"update\", \"delete\", \"exit\"");
+
+                type = Console.ReadLine();
+
+                switch (type)
+                {
+                    case create:
+                        var createReply = await client.SayHelloAsync(Create(type));
+                        Console.WriteLine("\nServer reply:\n\n" + createReply.Message);
+                        break;
+                    case read:
+                        var readReply = await client.SayHelloAsync(Read(type));
+                        Console.WriteLine("\nServer reply:\n\n" + readReply.Message);
+                        break;
+                    case update:
+                        var updateReply = await client.SayHelloAsync(Update(type));
+                        Console.WriteLine("\nServer reply:\n\n" + updateReply.Message);
+                        break;
+                    case delete:
+                        var deleteReply = await client.SayHelloAsync(Delete(type));
+                        Console.WriteLine("\nServer reply:\n\n" + deleteReply.Message);
+                        break;
+                    case exit:
+                        return;
+                }
+            }
+        }
+
+        static HelloRequest Create(string type)
+        {
+            Console.WriteLine("Type Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Type Surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Type Patronym:");
+            string patronym = Console.ReadLine();
+
+            Console.WriteLine("Type Gender (male or female):");
+            bool gender = Console.ReadLine().ToLower() == "female" ? true : false;
+
+            Console.WriteLine("Type Birthdate year:");
+            int year = 0;
+            Int32.TryParse(Console.ReadLine(), out year);
+
+            Console.WriteLine("Type Birthdate month:");
+            int month = 0;
+            Int32.TryParse(Console.ReadLine(), out month);
+
+            Console.WriteLine("Type Birthdate day:");
+            int day = 0;
+            Int32.TryParse(Console.ReadLine(), out day);
+
+            Console.WriteLine("Type \"yes\" if has children, otherwise \"no\":");
+            bool hasChildren = Console.ReadLine() == "yes" ? true : false;
+
+            HelloRequest request = new HelloRequest
+            {
+                Type = type,
+                Name = name,
+                Surname = surname,
+                Patronym = patronym,
+                Gender = gender,
+                Year = year,
+                Month = month,
+                Day = day,
+                HasChildren = hasChildren
+            };
+            return request;
+        }
+
+        static HelloRequest Update(string type)
+        {
+            Console.WriteLine("Type Id: [ Using nonexistant Id will create new entity ]");
+            int id = 0;
+            Int32.TryParse(Console.ReadLine(), out id);
+
+            Console.WriteLine("Type Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Type Surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Type Patronym:");
+            string patronym = Console.ReadLine();
+
+            Console.WriteLine("Type Gender (male or female):");
+            bool gender = Console.ReadLine().ToLower() == "female" ? true : false;
+
+            Console.WriteLine("Type Birthdate year:");
+            int year = 0;
+            Int32.TryParse(Console.ReadLine(), out year);
+
+            Console.WriteLine("Type Birthdate month:");
+            int month = 0;
+            Int32.TryParse(Console.ReadLine(), out month);
+
+            Console.WriteLine("Type Birthdate day:");
+            int day = 0;
+            Int32.TryParse(Console.ReadLine(), out day);
+
+            Console.WriteLine("Type \"yes\" if has children, otherwise \"no\":");
+            bool hasChildren = Console.ReadLine() == "yes" ? true : false;
+
+            HelloRequest request = new HelloRequest
+            {
+                Id = id,
+                Type = type,
+                Name = name,
+                Surname = surname,
+                Patronym = patronym,
+                Gender = gender,
+                Year = year,
+                Month = month,
+                Day = day,
+                HasChildren = hasChildren
+            };
+            return request;
+        }
+
+        static HelloRequest Read(string type)
+        {
+            return new HelloRequest { Type = type };
+        }
+
+        static HelloRequest Delete(string type)
+        {
+            int id = 0;
+
+            Console.WriteLine("Type the employee's id you need to delete:");
+            Int32.TryParse(Console.ReadLine(), out id);
+
+            return new HelloRequest { Type = type, Id = id };
         }
     }
 }
---
+++ AdvanticaGrpcServer/AdvanticaGrpcServer/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdvanticaGrpcServer
+{
+    public class Context : DbContext
+    {
+        private string server = string.Empty;
+        private string dBName = string.Empty;
+        private string credentials = string.Empty;
+        private string connectionString = string.Empty;
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        public Context(string connectionString)
+        {
+            this.connectionString = connectionString;
+            
+            Database.EnsureCreated();
+        }
+        public Context(string server, string dBName, string credentials)
+        {
+            this.server = server;
+            this.dBName = dBName;
+            this.credentials = credentials;
+            this.connectionString = string.Format("Server={0};Database={1};{2};", server, dBName, credentials);
+
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
